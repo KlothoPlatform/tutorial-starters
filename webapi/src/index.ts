97ef31ee61a7dd3fa4efec3378ef4c43f45e5713
ewucc <eric.wu@cloudcompiler.run>
2022-02-07 11:30:50 -0800

    Updating the route for json, adding the quotestore

--- webapi/src/index.ts
+++ webapi/src/index.ts
@@ -2,6 +2,10 @@ import * as express from 'express';
 
 const app: any = express();
 const router = express.Router();
+router.use(express.urlencoded({ extended: true }));
+router.use(express.json());
+
+let quoteStore = new Map<string, string>();
 
 router.get('/', async (req, res) => {
   res.send("Hello World!");
